8d1955f32df4aa4768869b3eb21407ea7068d0ca
Leonardo <leosampsousa@gmail.com>
2020-07-14 15:56:54 -0300

    notacao bean no repository

--- src/main/java/com/inventory/somapadawanjava01/repository/InventoryRepository.java
+++ src/main/java/com/inventory/somapadawanjava01/repository/InventoryRepository.java
@@ -1,9 +1,10 @@
 package com.inventory.somapadawanjava01.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.inventory.somapadawanjava01.model.Inventory;
-
+@Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 	long countByComplete(boolean complete);
 	
